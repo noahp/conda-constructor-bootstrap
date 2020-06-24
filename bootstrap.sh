0b0c654661a352d37b72260f8c05079fdf098f6b
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2020-06-24 09:30:49 -0400

    add comments, quiet the script a bit

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 CONDA_ENV_NAME=${CONDA_ENV_NAME:-constructor}
 
-conda create -n $CONDA_ENV_NAME 'python=3.8' 'conda'
+echo ">>> Creating conda env \"$CONDA_ENV_NAME\"..."
+conda create --quiet -n "$CONDA_ENV_NAME" 'python=3.8' 'conda'
 
 # use source activate since we're running in non-interactive mode
-source $(dirname $CONDA_EXE)/../etc/profile.d/conda.sh
-conda activate $CONDA_ENV_NAME
+source "$(dirname $CONDA_EXE)/../etc/profile.d/conda.sh"
+conda activate "$CONDA_ENV_NAME"
 
-pip install git+https://github.com/conda/constructor.git@a0f1b8edea46c0fa6991b598860ac4a53f3a3111
+# Install a commit of the constructor tool that seems to work for me; the latest
+# release at the moment, 3.0.0, doesn't support the `environment_file` key for
+# constructing an installer from an existing environment file
+echo ">>> Installing constructor..."
+pip install --quiet git+https://github.com/conda/constructor.git@a0f1b8edea46c0fa6991b598860ac4a53f3a3111
 
-echo "You now can run 'conda activate $CONDA_ENV_NAME' to access the constructor utility!"
+echo ">>> You now can run 'conda activate \"$CONDA_ENV_NAME\"' to access the constructor utility!"
